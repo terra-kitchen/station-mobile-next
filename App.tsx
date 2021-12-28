a9e665e6c5831512b42f518e5c466a8acf09c339
sim <sim@terra.money>
2021-12-28 19:16:23 +0900

    recoil

--- App.tsx
+++ App.tsx
@@ -1,6 +1,8 @@
 import "node-libs-react-native/globals"
 import "react-native-get-random-values"
 
+import React from "react"
+import { RecoilRoot } from "recoil"
 import { QueryClient, QueryClientProvider } from "react-query"
 
 import TerraJS from "./labs/TerraJS"
@@ -9,8 +11,10 @@ const queryClient = new QueryClient()
 
 export default function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <TerraJS />
-    </QueryClientProvider>
+    <RecoilRoot>
+      <QueryClientProvider client={queryClient}>
+        <TerraJS />
+      </QueryClientProvider>
+    </RecoilRoot>
   )
 }
--- labs/TerraJS.tsx
+++ labs/TerraJS.tsx
@@ -1,11 +1,17 @@
 import React from "react"
-import { Text } from "react-native"
+import { Text, View } from "react-native"
 import { useQuery } from "react-query"
+import { atom, useRecoilValue } from "recoil"
 import { Coins, LCDClient } from "@terra-money/terra.js"
 
-const address = "terra1ac2u4fekndumvj0ycutlahcf32se88prljf0m3"
+const addressState = atom({
+  key: "address",
+  default: "terra1ac2u4fekndumvj0ycutlahcf32se88prljf0m3",
+})
 
 export const useInitialBankBalance = () => {
+  const address = useRecoilValue(addressState)
+
   const lcd = new LCDClient({
     chainID: "columbus-5",
     URL: "https://lcd.terra.dev",
@@ -22,10 +28,13 @@ const TerraJS = () => {
   const { data: bankBalance } = useInitialBankBalance()
 
   return (
-    bankBalance
-      ?.toData()
-      .map((coin) => <Text key={coin.denom}>{coin.amount.toString()}</Text>) ??
-    null
+    <View>
+      {bankBalance
+        ?.toData()
+        .map((coin) => (
+          <Text key={coin.denom}>{coin.amount.toString()}</Text>
+        )) ?? null}
+    </View>
   )
 }
 
